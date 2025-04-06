654706097879ddf4f369ff7a3538e1fd4f359bf5
AnthonyA <alicea.9a@gmail.com>
2025-04-06 17:20:09 -0600

    feat: first attempent learning blocks in ruby to make benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
-  # Your benchmarking code goes here.
+  # Your benchmarking code goes here.w
+  time_start = Time.now
+  results_from_string = yield
+  time_start - Time.now
+  
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
